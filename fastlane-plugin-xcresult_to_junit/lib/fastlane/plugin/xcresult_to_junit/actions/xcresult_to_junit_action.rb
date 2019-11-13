6e58933412ae4a6d9990f8b2f6c66c5e2f028535
Shane Birdsall <shane.da.pain@hotmail.com>
2019-11-13 14:26:02 +1300

    Move some logic into helper file to make the action a bit easier to follow

--- fastlane-plugin-xcresult_to_junit/lib/fastlane/plugin/xcresult_to_junit/actions/xcresult_to_junit_action.rb
+++ fastlane-plugin-xcresult_to_junit/lib/fastlane/plugin/xcresult_to_junit/actions/xcresult_to_junit_action.rb
@@ -8,47 +8,31 @@ module Fastlane
     class XcresultToJunitAction < Action
       def self.run(params)
         UI.message("The xcresult_to_junit plugin has started!")
-        all_results = Helper::XcresultToJunitHelper.load_results(params)['actions']['_values']
-        
-        ## READ XCRESULT START
+        all_results = Helper::XcresultToJunitHelper.load_results(params[:xcresult_path])['actions']['_values']
         all_results.each do |test_run|
           if test_run['actionResult']['testsRef'] # Skip if section has no testRef data as this means its not a test run
             test_run_id = test_run['actionResult']['testsRef']['id']['_value']
-            device_destination = test_run['runDestination']
-            device_udid = device_destination['targetDeviceRecord']['identifier']['_value']
-            device_details = {
-              'udid' => device_udid,
-              'name' => device_destination['targetDeviceRecord']['modelName']['_value'],
-              'os' => device_destination['targetDeviceRecord']['operatingSystemVersion']['_value']
-            }.to_json
-
-            all_tests = Helper::XcresultToJunitHelper.load_object(params, test_run_id)['summaries']['_values'][0]['testableSummaries']['_values']
+            all_tests = Helper::XcresultToJunitHelper.load_object(params[:xcresult_path], test_run_id)['summaries']['_values'][0]['testableSummaries']['_values']
             test_suites = []
-            
             all_tests.each do |target|
               target_name = target['targetName']['_value']
-              # if the test target failed to launch at all, get first failure message
               unless target['tests']
                 failure_summary = target['failureSummaries']['_values'][0]
                 test_suites << { name: target_name, error: failure_summary['message']['_value'] }
                 next
               end
-              
               test_classes = target['tests']['_values'][0]['subtests']['_values'][0]['subtests']['_values']
               test_classes.each do |test_class|
                 suite = { name: "#{target_name}.#{test_class['name']['_value']}", cases: [] }
-                
                 if test_class['subtests']
                   test_class['subtests']['_values'].each do |test|
                     duration = 0
                     duration = test['duration']['_value'] if test['duration']
                     testcase = { name: test['name']['_value'], time: duration }
-                    
                     if test['testStatus']['_value'] == 'Failure'
-                      failure = Helper::XcresultToJunitHelper.load_object(params, test['summaryRef']['id']['_value'])['failureSummaries']['_values'][0]
+                      failure = Helper::XcresultToJunitHelper.load_object(ENV['XCRESULT_TO_JUNIT_XCRESULT_PATH'], test['summaryRef']['id']['_value'])['failureSummaries']['_values'][0]
                       filename = failure['fileName']['_value']
                       message = failure['message']['_value']
-                      
                       if filename == '<unknown>'
                         testcase[:error] = message
                       else
@@ -59,75 +43,34 @@ module Fastlane
                     suite[:cases] << testcase
                   end
                 end
-                
                 suite[:count] = suite[:cases].size
                 suite[:failures] = suite[:cases].count { |testcase| testcase[:failure] }
                 suite[:errors] = suite[:cases].count { |testcase| testcase[:error] }
                 test_suites << suite
               end
             end
-            ## READ XCRESULT END
-
-            ## OUTPUT JUNIT START
-            junit_folder = "#{params[:output_path]}/ios-#{device_udid}.junit"
-            FileUtils.rm_rf junit_folder
-            FileUtils.mkdir junit_folder
-            File.open("#{junit_folder}/device.json", 'w') do |f|
-              f << device_details
-            end
-            
-            old_stdout = $stdout
-            File.open("#{junit_folder}/results.xml", 'w') do |fo|
-              $stdout = fo
-              puts '<?xml version="1.0" encoding="UTF-8"?>'
-              puts '<testsuites>'
-              test_suites.each do |suite|
-                if suite[:error]
-                  puts "<testsuite name=#{suite[:name].encode xml: :attr} errors='1'>"
-                  puts "<error>#{suite[:error].encode xml: :text}</error>"
-                else
-                  puts "<testsuite name=#{suite[:name].encode xml: :attr} tests='#{suite[:count]}' failures='#{suite[:failures]}' errors='#{suite[:errors]}'>"
-                  suite[:cases].each do |testcase|
-                    print "<testcase classname=#{suite[:name].encode xml: :attr} name=#{testcase[:name].encode xml: :attr} time='#{testcase[:time]}'"
-                    if testcase[:failure]
-                      puts '>'
-                      puts "<failure message=#{testcase[:failure].encode xml: :attr}>#{testcase[:failure_location].encode xml: :text}</failure>"
-                      puts '</testcase>'
-                    elsif testcase[:error]
-                      puts '>'
-                      puts "<error>#{testcase[:error].encode xml: :text}</error>"
-                      puts '</testcase>'
-                    else
-                      puts '/>'
-                    end
-                  end
-                end
-                puts '</testsuite>'
-              end
-              puts '</testsuites>'
-            end
-            $stdout = old_stdout
+            junit_folder = Helper::XcresultToJunitHelper.save_device_details_to_file(params[:output_path], test_run['runDestination'])
+            Helper::XcresultToJunitHelper.generate_junit(junit_folder, test_suites)
           end
         end
-        ## OUTPUT JUNIT END
       end
-      
+
       def self.description
         "Produces junit xml files from Xcode 11+ xcresult files"
       end
-      
+
       def self.authors
         ["Shane Birdsall"]
       end
-      
+
       def self.return_value
         # If your method provides a return value, you can describe here what it does
       end
-      
+
       def self.details
         "By using the xcresulttool this plugin parses xcresult files and generates junit reports to be used with other tools to display iOS test results"
       end
-      
+
       def self.available_options
         [
           FastlaneCore::ConfigItem.new(key: :xcresult_path,
@@ -149,4 +92,3 @@ module Fastlane
         end
       end
     end
-    
--- fastlane-plugin-xcresult_to_junit/lib/fastlane/plugin/xcresult_to_junit/helper/xcresult_to_junit_helper.rb
+++ fastlane-plugin-xcresult_to_junit/lib/fastlane/plugin/xcresult_to_junit/helper/xcresult_to_junit_helper.rb
@@ -1,17 +1,105 @@
 require 'fastlane_core/ui/ui'
 require 'json'
+require 'fileutils'
 
 module Fastlane
   UI = FastlaneCore::UI unless Fastlane.const_defined?("UI")
 
   module Helper
     class XcresultToJunitHelper
-      def self.load_object(params, id)
-        JSON.load FastlaneCore::CommandExecutor.execute(command: "xcrun xcresulttool get --format json --path #{params[:xcresult_path]} --id #{id}")
+      def self.load_object(xcresult_path, id)
+        JSON.load FastlaneCore::CommandExecutor.execute(command: "xcrun xcresulttool get --format json --path #{xcresult_path} --id #{id}")
       end
 
-      def self.load_results(params)
-        JSON.load FastlaneCore::CommandExecutor.execute(command: "xcrun xcresulttool get --format json --path #{params[:xcresult_path]}")
+      def self.load_results(xcresult_path)
+        JSON.load FastlaneCore::CommandExecutor.execute(command: "xcrun xcresulttool get --format json --path #{xcresult_path}")
+      end
+
+      def self.save_device_details_to_file(output_path, device_destination)
+        device_udid = device_destination['targetDeviceRecord']['identifier']['_value']
+        device_details = {
+          'udid' => device_udid,
+          'name' => device_destination['targetDeviceRecord']['modelName']['_value'],
+          'os' => device_destination['targetDeviceRecord']['operatingSystemVersion']['_value']
+        }.to_json
+
+        junit_folder = "#{output_path}/ios-#{device_udid}.junit"
+        FileUtils.rm_rf junit_folder
+        FileUtils.mkdir junit_folder
+        File.open("#{junit_folder}/device.json", 'w') do |f|
+          f << device_details
+        end
+        return junit_folder
+      end
+
+      def self.junit_file_start
+        puts '<?xml version="1.0" encoding="UTF-8"?>'
+        puts '<testsuites>'
+      end
+
+      def self.junit_file_end
+        puts '</testsuites>'
+      end
+
+      def self.junit_suite_error(suite)
+        puts "<testsuite name=#{suite[:name].encode xml: :attr} errors='1'>"
+        puts "<error>#{suite[:error].encode xml: :text}</error>"
+      end
+
+      def self.junit_suite_start(suite)
+        puts "<testsuite name=#{suite[:name].encode xml: :attr} tests='#{suite[:count]}' failures='#{suite[:failures]}' errors='#{suite[:errors]}'>"
+      end
+
+      def self.junit_suite_end
+        puts '</testsuite>'
+      end
+
+      def self.junit_testcase_start(suite, testcase)
+        print "<testcase classname=#{suite[:name].encode xml: :attr} name=#{testcase[:name].encode xml: :attr} time='#{testcase[:time]}'"
+      end
+
+      def self.junit_testcase_success
+        puts '/>'
+      end
+
+      def self.junit_testcase_failure(testcase)
+        puts '>'
+        puts "<failure message=#{testcase[:failure].encode xml: :attr}>#{testcase[:failure_location].encode xml: :text}</failure>"
+        puts '</testcase>'
+      end
+
+      def self.junit_testcase_error(testcase)
+        puts '>'
+        puts "<error>#{testcase[:error].encode xml: :text}</error>"
+        puts '</testcase>'
+      end
+
+      def self.generate_junit(junit_folder, test_suites)
+        File.open("#{junit_folder}/results.xml", 'w') do |fo|
+          old_stdout = $stdout
+          $stdout = fo
+          Helper::XcresultToJunitHelper.junit_file_start()
+          test_suites.each do |suite|
+            if suite[:error]
+              Helper::XcresultToJunitHelper.junit_suite_error(suite)
+            else
+              Helper::XcresultToJunitHelper.junit_suite_start(suite)
+              suite[:cases].each do |testcase|
+                Helper::XcresultToJunitHelper.junit_testcase_start(suite, testcase)
+                if testcase[:failure]
+                  Helper::XcresultToJunitHelper.junit_testcase_failure(testcase)
+                elsif testcase[:error]
+                  Helper::XcresultToJunitHelper.junit_testcase_error(testcase)
+                else
+                  Helper::XcresultToJunitHelper.junit_testcase_success()
+                end
+              end
+            end
+            Helper::XcresultToJunitHelper.junit_suite_end()
+          end
+          Helper::XcresultToJunitHelper.junit_file_end()
+          $stdout = old_stdout
+        end
       end
     end
   end
