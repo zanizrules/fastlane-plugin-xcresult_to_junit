8dd4dc59087ce19f105a7bcf902ef07abb4357bc
Shane <shane.da.pain@hotmail.com>
2019-12-03 14:23:00 +1300

    Version 0.2.2 - Quick fix for varying unit test results

--- lib/fastlane/plugin/xcresult_to_junit/actions/xcresult_to_junit_action.rb
+++ lib/fastlane/plugin/xcresult_to_junit/actions/xcresult_to_junit_action.rb
@@ -36,18 +36,20 @@ module Fastlane
                     testcase = { name: testcase_name, time: duration }
                     map["#{suite_name}.#{testcase_name}"] = {'files' => [], 'tags' => tags}
 
-                    if test['summaryRef']
+                    if defined?(test['summaryRef']['id']['_value'])
                       summaryRef = test['summaryRef']['id']['_value']
-                      ref = Helper::XcresultToJunitHelper.load_object(params[:xcresult_path], summaryRef)['activitySummaries']['_values']
-                      ref.each do |summary|
-                        if summary['attachments']
-                          summary['attachments']['_values'].each do |attachment|
-                            timestamp = DateTime.parse(attachment['timestamp']['_value']).to_time.to_i
-                            name = attachment['name']['_value']
-                            folder_name = "#{suite_name}.#{testcase_name}"
-                            id = attachment['payloadRef']['id']['_value']
-                            Helper::XcresultToJunitHelper.fetch_screenshot(params[:xcresult_path], "#{junit_folder}/attachments/#{folder_name}", "#{id}.png", id)
-                            map[folder_name]['files'].push({'description' => name, 'mime-type' => 'image/png', 'path' => "#{folder_name}/#{id}.png", 'timestamp' => timestamp})
+                      ref = Helper::XcresultToJunitHelper.load_object(params[:xcresult_path], summaryRef)
+                      if defined?(ref['activitySummaries']['_values'])
+                        ref['activitySummaries']['_values'].each do |summary|
+                          if summary['attachments']
+                            summary['attachments']['_values'].each do |attachment|
+                              timestamp = DateTime.parse(attachment['timestamp']['_value']).to_time.to_i
+                              name = attachment['name']['_value']
+                              folder_name = "#{suite_name}.#{testcase_name}"
+                              id = attachment['payloadRef']['id']['_value']
+                              Helper::XcresultToJunitHelper.fetch_screenshot(params[:xcresult_path], "#{junit_folder}/attachments/#{folder_name}", "#{id}.png", id)
+                              map[folder_name]['files'].push({'description' => name, 'mime-type' => 'image/png', 'path' => "#{folder_name}/#{id}.png", 'timestamp' => timestamp})
+                            end
                           end
                         end
                       end
